b036bcf3c3b27693cecc2d4adbb3d9169d98636c
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2023-10-03 23:11:48 +0700

    integrate using simple API

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const qrcode = require("qrcode-terminal");
 const { Client } = require("whatsapp-web.js");
 const client = new Client();
 
+const http = require("./http");
+
 client.initialize();
 
 client.on("qr", (qr) => {
@@ -13,10 +15,17 @@ client.on("ready", () => {
   console.log("Client is ready!");
 });
 
-client.on("message", (msg) => {
+client.on("message", async (msg) => {
   console.log(msg);
 
   if (msg.body == "p") {
     msg.reply("q");
+  }
+
+  if (msg.body == "kalimat anime dong puh") {
+    const res = await http.get("/getrandom");
+    const item = res.data.result[0];
+
+    msg.reply(`"${item.indo}" \n ~ ${item.character} (${item.anime})`);
   }
 });
